708ed321278416d6cb430f00d03279ddb9380e18
CaraRota <canalsdigital@gmail.com>
2024-01-27 00:52:51 -0300

    Add Spinner component to display loading state

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
 import axios from "axios";
+import Spinner from "./ui/Spinner";
 
 const Homepage = () => {
     const [goldLoading, setGoldLoading] = useState(true);
@@ -26,21 +27,25 @@ const Homepage = () => {
     }, []);
     return (
         <>
-            <div className='border-t-amber-300 border-2 mt-5 py-10 px-7 rounded-md shadow-md w-52 h-28 mx-auto flex justify-center items-center flex-col'>
-                <h1 className='rounded-md text-3xl font-semibold text-gray-800 text-center'>
-                    Gold Price
-                </h1>
-                <div className='flex gap-2'>
-                    <p className='text-gray-800 text-xl'>{gold.price.toFixed(2)}</p>
-                    <img
-                        src='https://www.erepublik.net/images/modules/_icons/gold_24.png'
-                        alt='gold'
-                        className='mx-auto'
-                    />
+            {gold ? (
+                <div className='border-t-amber-300 border-2 mt-5 py-10 px-7 rounded-md shadow-md w-52 h-28 mx-auto flex justify-center items-center flex-col'>
+                    <h1 className='rounded-md text-3xl font-semibold text-gray-800 text-center'>
+                        Gold Price
+                    </h1>
+                    <div className='flex gap-2'>
+                        <p className='text-gray-800 text-xl'>{gold.price.toFixed(2)}</p>
+                        <img
+                            src='https://www.erepublik.net/images/modules/_icons/gold_24.png'
+                            alt='gold'
+                            className='mx-auto'
+                        />
+                    </div>
+                    <p className='text-gray-800'>{gold.amount} available</p>
+                    {/* <p className='text-gray-800'>{gold.citizen_name}</p> */}
                 </div>
-                <p className='text-gray-800'>{gold.amount} available</p>
-                {/* <p className='text-gray-800'>{gold.citizen_name}</p> */}
-            </div>
+            ) : (
+                <Spinner />
+            )}
             <div className='flex flex-wrap mx-auto max-w-screen-lg gap-2'>
                 <Card
                     apiUrl={"https://service.erepublik.tools/api/v1/market/item/0/7/1"}
