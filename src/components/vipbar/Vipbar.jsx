ee18899a29e38adc878148aba01c2a5a83323173
CaraRota <canalsdigital@gmail.com>
2024-12-15 11:53:42 -0300

    Refactor and updated vip bar prices

--- src/components/vipbar/Vipbar.jsx
+++ src/components/vipbar/Vipbar.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 
 import VipbarDetail from "./VipbarDetail";
+import { VIPBAR } from "../../constants";
 
 const Vipbar = ({ gold, frm, wrm, hrm, arm }) => {
+    const getData = (data) => {
+        if (!data) return null;
+        switch (data) {
+            case "gold":
+                return gold;
+            case "frm":
+                return frm;
+            case "wrm":
+                return wrm;
+            case "hrm":
+                return hrm;
+            case "arm":
+                return arm;
+            default:
+                return true;
+        }
+    };
     return (
         <>
             <div className='flex flex-col mx-auto w-11/12 lg:max-w-screen-lg gap-2 py-5 bg-gradient-to-b from-purple-100 to-purple-200 border-purple-300 mt-5 rounded-md border '>
@@ -10,135 +28,20 @@ const Vipbar = ({ gold, frm, wrm, hrm, arm }) => {
                     Vip Shop Prices
                 </div>
                 <div className='flex flex-row flex-wrap gap-2 justify-center items-center'>
-                    <VipbarDetail
-                        image={"https://www.erepublik.com/images/icons/boosters/128px/speed.png"}
-                        alt={"2x Damage Accelerator Icon"}
-                        text={"2x Damage Accelerators"}
-                        data={gold}
-                        amount={0.7}
-                        gold={true}
-                    />
-                    <VipbarDetail
-                        image={"https://www.erepublik.com/images/icons/boosters/128px/damage.png"}
-                        alt={"Ground Damage Icon"}
-                        text={"+50% Ground Damage"}
-                        data={frm}
-                        amount={77}
-                    />
-                    <VipbarDetail
-                        image={"https://www.erepublik.com/images/icons/rewards/128px/storage.png"}
-                        alt={"Storage Icon"}
-                        text={"+1000 Storage"}
-                        data={frm}
-                        amount={227}
-                    />
-                    <VipbarDetail
-                        image={"https://www.erepublik.com/images/icons/industry/1/q10_128x128.png"}
-                        alt={"Simple Energy Bar Icon"}
-                        text={"Simple Energy Bar"}
-                        data={gold}
-                        gold={true}
-                        amount={1.4}
-                    />
-                    <VipbarDetail
-                        image={"https://www.erepublik.com/images/icons/boosters/128px/catchup.png"}
-                        alt={"Ghost Booster Icon"}
-                        text={"Ghost Booster"}
-                        data={wrm}
-                        amount={56}
-                    />
-                    <VipbarDetail
-                        image={"https://www.erepublik.com/images/icons/industry/1/q11_128x128.png"}
-                        alt={"Double Energy Bar Icon"}
-                        text={"Double Energy Bar"}
-                        data={gold}
-                        gold={true}
-                        amount={3}
-                    />
-                    <VipbarDetail
-                        image={
-                            "https://www.erepublik.com/images/icons/rewards/128px/factory_reset_token.png"
-                        }
-                        alt={"Factory Reset Token Icon"}
-                        text={"Factory Reset Token"}
-                        data={hrm}
-                        amount={5}
-                    />
-                    <VipbarDetail
-                        image={
-                            "https://www.erepublik.com/images/icons/boosters/128px/deploy_size.png"
-                        }
-                        alt={"+100% Deploy Size Icon"}
-                        text={"+100% Deploy Size"}
-                        data={frm}
-                        amount={129}
-                    />
-                    <VipbarDetail
-                        image={
-                            "https://www.erepublik.com/images/icons/boosters/128px/deploy_rank_ground.png"
-                        }
-                        alt={"+10% Ground Rank Points Icon"}
-                        text={"+10% Ground Rank Points"}
-                        data={wrm}
-                        amount={142}
-                    />
-                    <VipbarDetail
-                        image={"https://www.erepublik.com/images/icons/rewards/128px/storage.png"}
-                        alt={"+9000 Storage Icon"}
-                        text={"+9000 Storage"}
-                        data={wrm}
-                        amount={262}
-                    />
-                    <VipbarDetail
-                        image={
-                            "https://www.erepublik.com/images/icons/boosters/128px/air_damage.png"
-                        }
-                        alt={"+20% Air Damage Icon"}
-                        text={"+20% Air Damage"}
-                        data={arm}
-                        amount={4}
-                    />
-                    <VipbarDetail
-                        image={"https://www.erepublik.com/images/icons/boosters/128px/damage.png"}
-                        alt={"+100% Ground Damage Icon"}
-                        text={"+100% Ground Damage"}
-                        data={wrm}
-                        amount={172}
-                    />
-                    <VipbarDetail
-                        image={
-                            "https://www.erepublik.com/images/icons/boosters/128px/deploy_rank_air.png"
-                        }
-                        alt={"+10% Air Rank Points Icon"}
-                        text={"+10% Air Rank Points"}
-                        data={arm}
-                        amount={9}
-                    />
-                    <VipbarDetail
-                        image={"https://www.erepublik.com/images/icons/boosters/128px/speed.png"}
-                        alt={"5x Damage Accelerators Icon"}
-                        text={"5x Damage Accelerators"}
-                        data={true}
-                        cc={4486}
-                    />
-                    <VipbarDetail
-                        image={
-                            "https://www.erepublik.com/images/icons/boosters/128px/deploy_rank_ground.png"
-                        }
-                        alt={"+20% Ground Rank Points Icon"}
-                        text={"+20% Ground Rank Points"}
-                        data={wrm}
-                        amount={337}
-                    />
-                    <VipbarDetail
-                        image={
-                            "https://www.erepublik.com/images/icons/boosters/128px/air_damage.png"
-                        }
-                        alt={"+50% Air Damage Icon"}
-                        text={"+50% Air Damage"}
-                        data={arm}
-                        amount={10}
-                    />
+                    {VIPBAR.map((vip, index) => {
+                        return (
+                            <VipbarDetail
+                                key={index}
+                                image={vip.image}
+                                alt={vip.alt}
+                                text={vip.text}
+                                data={getData(vip.data)}
+                                amount={vip.amount}
+                                gold={vip.gold}
+                                cc={vip.cc}
+                            />
+                        );
+                    })}
                 </div>
             </div>
         </>
--- src/components/vipbar/VipbarDetail.jsx
+++ src/components/vipbar/VipbarDetail.jsx
@@ -9,7 +9,12 @@ const VipbarDetail = ({ image, alt, text, data, amount, gold = null, cc = null }
     return (
         !loading &&
         data && (
-            <Tooltip title={text} arrow placement='top'>
+            <Tooltip
+                title={`${text} = ${amount ?? cc} ${
+                    data === true ? "CC" : data.industry ? data.industry : "gold"
+                } * $${cc ? cc : gold ? data.price : data.offers[0].gross}`}
+                arrow
+                placement='top'>
                 <div className='border-t-purple-400 bg-purple-50 hover:bg-purple-100 border border-t-2 shadow-sm mt-5 px-2 rounded-lg w-28 h-10 flex flex-col justify-center items-center cursor-pointer'>
                     <div className='flex flex-row gap-2 items-center justify-center text-center'>
                         <img src={image} alt={alt} className='mx-auto size-8' />
